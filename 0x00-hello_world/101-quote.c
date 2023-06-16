a8a7220535b9de3009393e48f295e1b44b70e6d7
JillNyamu <nyamujill@gmail.com>
2023-06-16 01:19:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 cat > 101-quote.c
-
 #include <unistd.h>
 /**
  * main - 101-qoute.c
@@ -14,6 +13,5 @@ int main(void)
         write(2,
               "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
               59);
-
         return (1);
 }
