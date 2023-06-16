58a2c5440d32674504d2bef5aaf2e66a1b9b8540
JillNyamu <nyamujill@gmail.com>
2023-06-16 04:36:10 -0700

    5-printf.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-cat > 101-quote.c
 #include <unistd.h>
 /**
  * main - 101-qoute.c
@@ -10,8 +9,8 @@ cat > 101-quote.c
  */
 int main(void)
 {
-        write(2,
-              "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-              59);
-        return (1);
+	write(2
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
+	return (1);
 }
